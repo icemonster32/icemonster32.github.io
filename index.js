ad5897ed4ed7e537a4a28a1e3d4ef6a58699df11
Nicolo Galinato <nicologalinato80@gmail.com>
2022-08-23 05:46:03 +0800

    player and bot
    player vs bot

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
+/* Game function */
 game = () => {
+   /* Query selectors */
+   const rockBtn = document.querySelector(".rock")
+   const paperBtn = document.querySelector(".paper")
+   const scissorBtn = document.querySelector(".scissor")
 
-    const arrList = ['Rock', 'Paper', 'Scissor',]
+   /* Array player and bot */
+   const arrPlayer = [rockBtn, paperBtn, scissorBtn]
+   const arrBot = ["Rock", "Paper", "Scissor"]
 
-    const getRandom = (arr) => {
-        const randomIndex = Math.floor(Math.random() * arr.length)
+   /* For each arrow path function */
+   arrPlayer.forEach((option) => {
+      /* Onclick event function */
+      option.onclick = () => {
+         const randomIndex = Math.floor(Math.random() * arrBot.length)
+         const item = arrBot[randomIndex]
+         const demo = (document.querySelector("#demo").innerHTML = item)
 
-        const item =  arr[randomIndex]
+         winner(option.innerHTML, demo)
+      }
 
-        return item
-    }
+      winner = (player, bot) => {
 
-    const result = getRandom(arrList)
-    console.log(result)
+         /* Determine the winner */
+         if (player === bot) {
+            document.querySelector("#demo2").innerHTML = "Tie"
+         } else if (player == "Rock") {
+            if (bot == "Paper") {
+               document.querySelector("#demo2").innerHTML = "Bot Win"
+            } else {
+               document.querySelector("#demo2").innerHTML = "Player Win"
+            }
+         } else if (player == "Scissor") {
+            if (bot == "Rock") {
+               document.querySelector("#demo2").innerHTML = "Bot Win"
+            } else {
+               document.querySelector("#demo2").innerHTML = "Player Win"
+            }
+         } else if (player == "Paper") {
+            if (bot == "Scissor") {
+               document.querySelector("#demo2").innerHTML = "Bot Win"
+            } else {
+               document.querySelector("#demo2").innerHTML = "Player Win"
+            }
+         }
+      }
+   })
 }
 game()
